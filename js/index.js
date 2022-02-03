56ffdd0bf3e7d90400405a6e3316ee224247753e
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-02-03 23:06:42 +0700

    mobile layout unfinished draft

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function showResNavMenu() {
+    document.getElementById("resNavMenu").style.display = "block";
+}
+
+function hideResNavMenu() {
+    document.getElementById("resNavMenu").style.display = "none";
+}
